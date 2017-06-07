07edc8c7b8906e554ff3759c8c4c3a66af11bf95
Jack Sowter <jhsowter@gmail.com>
2017-06-07 22:28:49 +1000

    try routing

--- source/src/app/app.module.ts
+++ source/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog.component';
 import { MarkdownModule  } from 'angular2-markdown';  
+
+const appRoutes = [
+  { 
+    path: 'post/:id',      
+    component: BlogComponent 
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
-    MarkdownModule
+    MarkdownModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ source/src/app/blog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'blog',
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.css']
+})
+export class BlogComponent {
+  title = 'app';
+  blog = "assets/20170606.md";
+
+
+}
