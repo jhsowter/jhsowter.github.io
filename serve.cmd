b25b06b0d55aef3b669b0a03e9708b9e6e90f97c
Jack Sowter <jhsowter@gmail.com>
2017-07-17 16:40:05 +1000

    time to read

--- serve.cmd
+++ serve.cmd
@@ -1,1 +1,1 @@
-docker run --rm -v %cd%:/srv/jekyll -p 4000:4000 jekyll/jekyll jekyll serve --watch --force_polling --drafts
+docker run -it -v %cd%:/srv/jekyll -p 4000:4000 jekyll/jekyll jekyll serve --watch --force_polling --drafts
